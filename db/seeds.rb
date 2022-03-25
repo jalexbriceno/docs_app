9046d4a8ef4a6415ddde07bd99e7236c23015024
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-03-25 09:53:25 -0600

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,16 @@ speciality = ['teacher', 'ta', 'student']
     last_name: Faker::Name.last_name,
     
   )
-
- 
   10.times do 
     user = User.create(
       first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name
+      last_name: Faker::Name.last_name,
       phone: "(888) 545-7575"
     )
 
     Appointment.create(
-      time: Faker::Timer.between(from: 'October 15, 2018 10:48 AM')
-      date: Faker::Date.between(from: '2022-01-01', to: '2022-12-31')
+      time: Faker::Time.between(from: 'October 15, 2018 10:48 AM'),
+      date: Faker::Date.between(from: '2022-01-01', to: '2022-12-31'),
       user_id: user.id,
       doctor_id: doctor.id
     )

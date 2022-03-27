c2630541c5dd493fb03b2e39271dae6cbc51c782
Spencer Savage <spencer.savage11@gmail.com>
2022-03-27 14:24:03 -0600

    fixed appointments foler patterns, added appointmentprovider.js

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_many :doctors, through: :appointments
+
+  validates :first_name, :last_name, :phone, :notes, presence: true
 end

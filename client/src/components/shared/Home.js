e1b9de8359272fe5f0d780ff426ebfdceed48269
Alex Briceno <jalexbriceno@gmail.com>
2022-03-30 22:05:30 -0600

    accordion

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -2,8 +2,9 @@ import DrPng from '../../images/Dr.png';
 import MedPng from '../../images/med.png';
 import TwitterSvg from '../../images/twitter.svg';
 import { MainHeader, MainHeaderText, SubHeader} from '../styles/sharedStyles';
-import {Row, Col, Container, Jumbotron} from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import {Row, Col, Container, Accordion } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 
 
@@ -32,8 +33,32 @@ const Home = () => (
           </MainHeaderText>
         </Row>
       </MainHeader>
-    
+      <h3>FAQs</h3>
+      
+    {/* create own accordion */}
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>How much does it cost?</Accordion.Header>
+        <Accordion.Body>
+        All of the cost will be covered by all insurance companies and our rich benefactors and sponsors to keep the cost free and provide a wide range of access to healthcare.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>What care do you provide?</Accordion.Header>
+        <Accordion.Body>
+        We provide care all the way from pediatrics, to family doctors. From heads to toes, in and out, we provide all the care needed for patients and link them up to the correct needed specialist.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>How do I make a appointment?</Accordion.Header>
+        <Accordion.Body>
+        To make a appointment, all you need is to call the number and provide the receptionist the info needed and you are all set! We will put you in our system and connect everything for you. It is that easy!
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   </>
+
 )
 
 export default Home;
+    

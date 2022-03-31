e61d8c84d58e9efdc35d83b13b726ca94f4be1e3
Spencer Savage <spencer.savage11@gmail.com>
2022-03-31 17:45:23 -0600

    users styling

--- client/src/components/doctors/DoctorList.js
+++ client/src/components/doctors/DoctorList.js
@@ -12,7 +12,11 @@ const DoctorList = () => (
            { value.doctors.map( d => 
              <Col xs={6} md={3}>
                <Card style={{ width: '14rem', backgroundColor: '#64FCD9'}}>
-                 <Card.Img variant="top" src="https://images.unsplash.com/photo-1643297654416-05795d62e39c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80" alt="doctor" />
+                 <Card.Img 
+                  variant="top" 
+                  src="https://images.unsplash.com/photo-1643297654416-05795d62e39c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80" 
+                  alt="doctor" 
+                  />
                  <Card.Body>
                    <Card.Title>Dr. {d.first_name} {d.last_name}</Card.Title>
                  </Card.Body>
--- client/src/components/users/UserList.js
+++ client/src/components/users/UserList.js
@@ -10,16 +10,20 @@ const UserList = () => (
         <Container>
           <Row>
             { value.users.map( u => 
-              <Col xs={6} md={5}>
-                <Link to={`/users/${u.id}`}>
-                  <Card key={u.id} style={{width: '12rem'}}>
-                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1647904522209-9e15687ecb9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="user">
-                    </Card.Img>
-                    <Card.Body>
-                      <Card.Title>{u.first_name} {u.last_name}</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Link>
+              <Col xs={6} md={3}>
+                <Card key={u.id} style={{width: '14rem', backgroundColor: '#FFB61D'}}>
+                  <Card.Img 
+                    variant="top" 
+                    src="https://images.unsplash.com/photo-1647904522209-9e15687ecb9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" 
+                    alt="user"
+                  />
+                  <Card.Body>
+                    <Card.Title>{u.first_name} {u.last_name}</Card.Title>
+                  </Card.Body>
+                  <Link to={`/users/${u.id}`}>
+                    <button style={{width:'100%', backgroundColor: '#FFB61D'}} variant="primary">Show</button>
+                  </Link>
+                 </Card>
               </Col>
               )}
           </Row>
